d6a127e6db83be8512212d14c274321f82ad2d41
Justin Chong <justinchong753@gmail.com>
2025-09-24 20:02:31 -0400

    changed copy on landing page

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
       <Navbar />
       <div className='Intro'>
         <Typewriter />
-        <h2><span class="blueText">Product designer </span>@ MongoDB passionate about creating inclusive & accessible technologies </h2>
+        <h2><span class="blueText">Data-driven product designer</span> turning complex ambiguity into simplicity across B2B and B2C</h2>
+        <h2 id='miniDescription'>Delivering GenAI and Go-to-Market solutions @ MongoDB to drive customer success and revenue. I believe great design has the power to make the digital world inclusive for everyone.</h2>
         <a href="https://www.linkedin.com/in/justin-r-chong/" target="_blank" rel="noopener noreferrer">
           <img id="linkedinLogo" src={linkedinLogo} alt="Linkedin Logo" width='22' height='22'/></a>
         <h3><a href="https://www.linkedin.com/in/justin-r-chong/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h3>
